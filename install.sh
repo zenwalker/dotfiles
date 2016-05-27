0fdde0852bbfdf197d199d4ec54b3bc82810e00d
Maxim Poletaev <max.poletaev@gmail.com>
2016-05-27 13:51:39 +0300

    update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s $SOURCE/vimrc ~/.vimrc
 ln -s $SOURCE/zshrc ~/.zshrc
 ln -s $SOURCE/gemrc ~/.gemrc
 ln -s $SOURCE/sublime-text ~/.config/sublime-text-3/Packages/User
+ln -s $SOURCE/atom ~/.atom
 
 if [ -d ~/.oh-my-zsh ]; then
     ln -s $SOURCE/oh-my-zsh ~/.oh-my-zsh/custom
